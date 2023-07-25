c5450f307aba60f93cc39b29fd872dc515e91bd7
lukrids <lukasmuellerds@gmail.com>
2023-07-25 15:42:52 +0100

    first init

---
+++ src/com/qa/Bookstore.java
@@ -0,0 +1,29 @@
+package com.qa;
+import java.util.*;
+
+public class Bookstore {
+    private final Book[] listBooks = new Book[20];
+    private int numberBooks;
+    public void addBook(Book newBook){
+        listBooks[numberBooks] = newBook;
+        numberBooks += 1;
+    }
+
+    private Boolean checkName(String name, String bookName) {
+        return name.equals(bookName);
+    }
+
+    public Boolean checkAvailable(String name) {
+        for (int i = 0; i < numberBooks; i++) {
+            if (checkName(name, listBooks[i].getName())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public Bookstore() {
+        numberBooks = 0;
+    }
+}
