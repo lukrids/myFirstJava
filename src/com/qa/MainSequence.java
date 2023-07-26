60175464ee6404f56bd1b030f50830144ce5332b
lukrids <lukasmuellerds@gmail.com>
2023-07-26 14:28:13 +0100

    add animals to project

---
+++ src/com/qa/MainSequence.java
@@ -0,0 +1,15 @@
+package com.qa;
+import com.qa.model.*;
+
+public class MainSequence {
+
+    Cat c = new Cat("Whiskers", 10);
+    Cat c2 = new Cat("Midas", 8);
+    Dog d = new Dog("Spot", 2);
+    Rabbit r = new Rabbit("Peter", 2);
+    Rabbit r2 = new Rabbit("Cottontail", 3);
+
+    public static void main(String[] args) {
+
+    }
+}
