33607209797aadb95299743c9f4326d355a8bd7d
lukrids <lukasmuellerds@gmail.com>
2023-07-26 15:00:04 +0100

    save all the latest stuff, of the animal excersice

--- src/com/qa/MainSequence.java
+++ src/com/qa/MainSequence.java
@@ -1,6 +1,6 @@
 package com.qa;
 import com.qa.model.*;
-import java.util.*
+import java.util.*;
 
 public class MainSequence {
 
@@ -10,7 +10,7 @@ public class MainSequence {
     Rabbit r = new Rabbit("Peter", 2);
     Rabbit r2 = new Rabbit("Cottontail", 3);
 
-    public void testArrayList() {
+    public ArrayList<Animal> getAnimalList() {
         ArrayList<Animal> animalList = new ArrayList<Animal>();
 
         animalList.add(c);
@@ -18,6 +18,24 @@ public class MainSequence {
         animalList.add(d);
         animalList.add(r);
         animalList.add(r2);
+
+        return animalList;
+    }
+
+    public HashMap<String, Animal> gethashMap() {
+        HashMap<String, Animal> animalMap = new HashMap<String, Animal>();
+
+        animalMap.put(c.getName(), c);
+        animalMap.put(c2.getName(), c2);
+        animalMap.put(d.getName(), d);
+        animalMap.put(r.getName(), r);
+        animalMap.put(r2.getName(), r2);
+
+        return animalMap;
+    }
+
+    public void testArrayList() {
+        ArrayList<Animal> animalList = getAnimalList();
 
         for (int x = 0; x < animalList.size(); x++){
             System.out.println(animalList.get(x));
@@ -44,12 +62,7 @@ public class MainSequence {
 
     public void testSet() {
 
-        ArrayList<Animal> animalList = new ArrayList<Animal>();
-        animalList.add(c);
-        animalList.add(c2);
-        animalList.add(d);
-        animalList.add(r);
-        animalList.add(r2);
+        ArrayList<Animal> animalList = getAnimalList();
 
         HashSet<Animal> set = new HashSet<>();
         //we can just add all the elements of another collection
@@ -65,7 +78,36 @@ public class MainSequence {
 
     }
 
+    public void searchForObject() {
+
+        ArrayList<Animal> animalList = getAnimalList();
+        HashMap<String, Animal> animalMap = gethashMap();
+
+        System.out.println("\nFinding Spot.");
+        for (Animal a : animalList){
+            if (a.getName().equals("Spot")){
+                System.out.println(
+                        "Found Spot in the ArrayList: " + a);
+            }
+        }
+
+        System.out.println("Found Spot in the HashMap: " +
+                animalMap.get("Spot"));
+
+    }
+
+    public void testSort() {
+        ArrayList<Animal> animalList = getAnimalList();
+
+        Collections.sort(animalList);
+        System.out.println("Sorted animal list");
+        for (int x = 0; x < animalList.size(); x++){
+            System.out.println(animalList.get(x));
+        }
+    }
+
     public static void main(String[] args) {
+        // add some stuff, by calling the right function
 
     }
 }
