8085c88e9ef66c4ebdd66829bd811f84217f67d9
lukrids <lukasmuellerds@gmail.com>
2023-07-26 10:42:42 +0100

    add circle and interface to this

---
+++ src/com/qa/Circle.java
@@ -0,0 +1,31 @@
+package com.qa;
+
+public class Circle extends Shape implements Movable{
+    private double radius;
+
+    public Circle(String name, String colour, double x, double y, double radius) {
+        super(name, colour, x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public Point getCurrentLocation() {
+        return getCentrePoint();
+    }
+
+    @Override
+    public void move(double x, double y) {
+        setX(getX() + x);
+        setY(getY() + y);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public Point getCentrePoint() {
+        return new Point(getX(), getY());
+    }
+}
--- src/com/qa/Main.java
+++ src/com/qa/Main.java
@@ -16,6 +16,20 @@ public class Main {
                 + r2.getCentrePoint() + ", Area is: " + r2.getArea()
                 + ". Is it square? " + r2.isSquare());
 
+
+        Circle c = new Circle ("circle1", "red", 0,0, 10);
+        Circle c2 = new Circle ("circle2", "blue", 10,10, 5);
+
+        System.out.println(c);
+        System.out.println(c2);
+
+        System.out.println("Circle location before move: " + c.getCurrentLocation());
+
+        c.move(10.5, 4.3);
+
+        System.out.println("Circle location after move: " + c.getCurrentLocation());
+
+
     }
 
 }
